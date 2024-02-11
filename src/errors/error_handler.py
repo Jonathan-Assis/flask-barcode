e7e05d6cb21a0edf49180b94f787945b8202229c
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2024-02-11 19:48:32 -0300

    Added cerberus to validate body and send custom exception

--- src/errors/error_handler.py
+++ src/errors/error_handler.py
@@ -1,10 +1,19 @@
 from src.views.http_types.http_response import HttpResponse
+from .error_types.http_unprocessable_entity import HttpUnprocessableEntityError
 
 def handle_errors(error: Exception) -> HttpResponse:
+    if isinstance (error, HttpUnprocessableEntityError):
+        body = {
+                "errors": [{
+                "title": error.name,
+                "detail": error.message}] 
+        }
+
+        return HttpResponse(status_code=error.status_code, body=body)
     body = {
         "errors": [{
             "title": "Server Error",
-            "detail": str(error)
-        }]
+            "detail": str(error)}]
     }
+
     return HttpResponse(status_code=500,body=body)
